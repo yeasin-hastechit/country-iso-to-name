6081571079ed353440dc9e36129263579551c019
Yeasin01 <71247390+yeasin-hastechit@users.noreply.github.com>
2023-08-27 13:50:49 +0600

    Initial commit

---
+++ src/js/main.js
@@ -0,0 +1,57 @@
+import '../css/style.css'
+import equals from '../images/equals.svg';
+
+import Country from 'country-list-js';
+
+const App = document.getElementById('app');
+
+
+App.innerHTML = `
+  <div class="container max-w-screen-xl mx-auto py-24">
+    <div class="flex flex-wrap gap-6">
+      <div class="flex-1">
+        <h2 class="text-2xl mb-3">Input</h2>
+        <textarea id="input_field" class=" border border-gray-300 resize-none w-full h-80 px-5 py-4" placeholder="Type your Country ISO code here."></textarea>
+        <p class="text-gray-500">Please use <b>iso2</b> or <b>iso3</b> country code. You can also use both <b>iso2</b> and <b>iso3</b> code. Each country code need to be in a new line.</p>
+      </div>
+      <div class="w-10 flex justify-center items-center"><img src="${equals}" class=" w-5" /></div>
+      <div class="flex-1">
+        <h2 class="text-2xl mb-3">Output</h2>
+        <textarea id="output_field" class=" border border-gray-300 resize-none w-full h-80 px-5 py-4" placeholder="Country name output."></textarea>
+      </div>
+    </div>
+  </div>
+`
+
+const inputField = App.querySelector('#input_field');
+const outputField = App.querySelector('#output_field');
+
+inputField.addEventListener('input', function() {
+  if(this.value === '') {
+    return outputField.value = '';
+  }
+  const countryNames = this.value.split('\n').map(code => {
+    let country
+    if(code.length === 3) {
+      country = Country.findByIso3(code.toUpperCase())
+    }
+    if(code.length === 2) {
+      country = Country.findByIso2(code.toUpperCase())
+    }
+    if(code === '') {
+      return `Empty space is not a valid iso2 or iso3 country code`;
+    }
+    if(country === undefined) {
+      return `${code} is not a valid iso2 or iso3 country code`;
+    }
+    return country.name
+  })
+  const uniqueCountry = [...new Set(countryNames)].map(name => {
+    const count = countryNames.filter(country => country === name).length
+    if(count > 1) {
+      return `${name} (${count})`
+    }
+    return name
+  }).join('\n');
+  outputField.value = uniqueCountry;
+})
---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig(({ command, mode }) => ({
+    base: '',
+    css: {
+        preprocessorOptions: {
+          // Specify the entry point to your main CSS file
+          // Adjust the path according to your project structure
+          // For example, if your CSS file is in src/css/styles.css
+          // then use: `src/css/styles.css`
+            module: './src/css/style.css',
+        },
+    },
+}));
